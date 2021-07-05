956c950a2c9b0d1318e683d7aa571d49f0db9c75
Sergio Melgarejo <sergiomelgarejo2@gmail.com>
2021-07-05 18:01:07 -0400

    Initial commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Sergio\Desktop\Proyectos Dev\django-blog\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
